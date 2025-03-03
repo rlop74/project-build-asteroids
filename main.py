4617e9eecaf1da6128b4f13a4c003aed2347338b
rlop74 <119091421+rlop74@users.noreply.github.com>
2025-03-02 21:41:13 -0500

    CH2: Player L7:Moving -- got the player ship to move around the screen via keys wasd

--- main.py
+++ main.py
@@ -3,9 +3,14 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) # instantiate Player object
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -19,9 +24,17 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        # player rotation
+        player.update(dt)
+        
+        screen.fill("black") # black background
+        player.draw(screen)
+        pygame.display.flip() # update the display
+        
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
-        screen.fill((0, 0, 0))
-        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
