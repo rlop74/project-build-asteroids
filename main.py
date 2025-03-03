863a2039f2b02aae15d45317a5165e09eb927d20
rlop74 <119091421+rlop74@users.noreply.github.com>
2025-03-02 23:15:41 -0500

    CH2: Player L8: Groups -- created Group class container to manage multiple game objects

--- main.py
+++ main.py
@@ -6,16 +6,19 @@ from constants import *
 from player import Player
 
 def main():
-    pygame.init()
-    clock = pygame.time.Clock()
+    pygame.init() # initialize pygame
+    clock = pygame.time.Clock() # create a clock for framerate control
+    updatable = pygame.sprite.Group() # group for updating objects
+    drawable = pygame.sprite.Group() # group for drawable objects
+    Player.containers = (updatable, drawable) # Assign sprite groups to the Player class
     dt = 0
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) # instantiate Player object
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) # instantiate Player object / creating Player instance
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    # create a screen (window)
+    # create a game screen or window
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     # game loop
@@ -26,10 +29,14 @@ def main():
                 return
         
         # player rotation
-        player.update(dt)
+        updatable.update(dt)
         
         screen.fill("black") # black background
-        player.draw(screen)
+        
+        # draw triangle ship
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip() # update the display
         
         # limit the framerate to 60 FPS
