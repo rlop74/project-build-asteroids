60e97edcccfdf05bffc7579e05dc6ace1f3098a5
rlop74 <119091421+rlop74@users.noreply.github.com>
2025-03-03 22:02:54 -0500

    CH3: Asteroids L1: Asteroids -- added moving asteroids

--- main.py
+++ main.py
@@ -4,12 +4,18 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init() # initialize pygame
     clock = pygame.time.Clock() # create a clock for framerate control
     updatable = pygame.sprite.Group() # group for updating objects
     drawable = pygame.sprite.Group() # group for drawable objects
+    asteroids = pygame.sprite.Group() # group for asteroid objects
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
     Player.containers = (updatable, drawable) # Assign sprite groups to the Player class
     dt = 0
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) # instantiate Player object / creating Player instance
